f14aea316044adef9e8518763b1d0426e2375ffa
arjunpotter17 <arjunpotter17@gmail.com>
2024-08-26 18:04:39 +0530

    added env to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    HELIUS_API_KEY: "ea9c561f-0680-4ae5-835c-5c0e463fa5e4",
+  },
+};
 
 export default nextConfig;
--- src/app/utils/helpers.tsx
+++ src/app/utils/helpers.tsx
@@ -1,22 +1,24 @@
 import { Connection } from "@solana/web3.js";
 
+// Fetcher function to use with SWR or similar data-fetching libraries
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+// Function to get Solana connection using the environment variable
 export const getConnection = () => {
-  //connection key should move to .env
-  const connection = new Connection(
-    `https://mainnet.helius-rpc.com/?api-key=${process.env.REACT_APP_HELIOUS_API_KEY}`
-  );
-  return connection;
+  const apiKey = process.env.HELIUS_API_KEY; // Make sure to use the correct environment variable
+  // Create a new Solana connection with the provided API key
+  return new Connection(`https://mainnet.helius-rpc.com/?api-key=${apiKey}`);
 };
 
-export const getEndpoint = (network:string) => {
+// Function to get the RPC endpoint for the specified network
+export const getEndpoint = (network: string) => {
+  const apiKey = process.env.HELIUS_API_KEY; // Make sure to use the correct environment variable
   switch (network) {
     case "mainnet":
-      return `https://mainnet.helius-rpc.com/?api-key=${process.env.REACT_APP_HELIOUS_API_KEY}`;
+      return `https://mainnet.helius-rpc.com/?api-key=${apiKey}`;
     case "devnet":
-      return `https://devnet.helius-rpc.com/?api-key=${process.env.REACT_APP_HELIOUS_API_KEY}`;
-    default:  
-      return `https://mainnet.helius-rpc.com/?api-key=${process.env.REACT_APP_HELIOUS_API_KEY}`;
+      return `https://devnet.helius-rpc.com/?api-key=${apiKey}`;
+    default:
+      return `https://mainnet.helius-rpc.com/?api-key=${apiKey}`;
   }
-}
+};
